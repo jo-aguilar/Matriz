4f2d699a2de031911599bba0825daceb2d92fb0a
jo-aguilar <joaoaguilar93@gmail.com>
2025-12-20 08:04:48 -0300

    Adição e subtração adicionadas

--- matriz.c
+++ matriz.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct Matriz Matriz;
 
@@ -15,6 +17,9 @@ Matriz matriz(double* mat, int linhas, int colunas);
 double ret(Matriz* self, int l, int c);
 Matriz transposta(Matriz *self);
 void print(Matriz *self);
+Matriz pm(Matriz*, Matriz*, char*);
+Matriz sm (Matriz* m1, Matriz* m2){ return pm(m1, m2, "mais");}
+Matriz sub(Matriz* m1, Matriz* m2){ return pm(m1, m2, "menos");}
 
 Matriz matriz(double* mat, int linhas, int colunas) {
 //Construtor do pseudo-objeto Matriz
@@ -27,17 +32,17 @@ Matriz matriz(double* mat, int linhas, int colunas) {
 	return m;
 }
 
+
 int main(){
-	double matx[3][3] = {
-		{1,2,3},{4,5,6},{7,8,9}
-	};
-	Matriz matj = matriz(&matx[0][0], 3, 3);
-	printf("Tamanho da linha: %i\n", matj.linhas);
-	printf("Tamanho da coluna: %i\n", matj.colunas);
-	printf("[][]: %f\n", matj.ret(&matj, 1,1));
-	matj.print(&matj);
-	Matriz nova = transposta(&matj);
-	nova.print(&nova);
+	
+	double m1[2][2] = {{1, 2}, {3,4}};
+	double m2[2][2] = {{5, 6}, {7,8}};
+	Matriz m11 = matriz(&m1[0][0], 2, 2);
+	Matriz m22 = matriz(&m2[0][0], 2, 2);
+	Matriz res1 = sm(&m11, &m22);
+	Matriz res2 = sub(&m11, &m22);
+	res1.print(&res1);
+	res2.print(&res2);
 }
 
 double ret(Matriz* self, int l, int c){
@@ -73,3 +78,25 @@ Matriz transposta(Matriz* self){
 	return matriz(nova_mat, c, l);
 }
 
+Matriz pm (Matriz* m1, Matriz* m2, char* op){
+	if(m1->linhas == m2->linhas && m1->colunas==m2->colunas){
+		const int l = m1->linhas;
+		const int c = m1->colunas;
+		double* r = (double*) malloc(l*c*sizeof(double));
+		for(int i = 0; i < l; i++){
+			for(int j = 0; j < c; j++){
+				if(strcmp(op, "mais")==0){
+					r[i*c + j] = m1->ret(m1, i, j) + m2->ret(m2, i, j);}
+				else if (strcmp(op, "menos")==0){
+					r[i*c + j] = m1->ret(m1, i, j) - m2->ret(m2, i, j);}
+				}	
+			}
+		return matriz(r, m1->linhas, m1->colunas);
+	}
+	else {
+		//Erro: Matrizes de tamanhos diferentes
+		//Retorna uma matriz |0
+		double m[1][1] = {0};
+		return matriz(&m[0][0], 1, 1);
+	}				       
+}
